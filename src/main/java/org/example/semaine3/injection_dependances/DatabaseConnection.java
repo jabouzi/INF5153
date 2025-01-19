51339890bbbf3a523b42545bff3c433be360c9cb
Skander Jabouzi <jabouzi@gmail.com>
2025-01-19 15:52:11 -0500

    Revert "ajouter les exemples de la semaine 3 - partie 1"

--- src/main/java/org/example/semaine3/injection_dependances/DatabaseConnection.java
+++
@@ -1,7 +0,0 @@
-package org.example.semaine3.injection_dependances;
-
-public interface DatabaseConnection {
-    void connect();
-    void disconnect();
-    String executeQuery(String query);
-}
--- src/main/java/org/example/semaine3/strucure_donnees/Point.java
+++
@@ -1,7 +0,0 @@
-package org.example.semaine3.strucure_donnees;
-
-//Structure de donnée:
-class Point {
-    public int x;
-    public int y;
-}
